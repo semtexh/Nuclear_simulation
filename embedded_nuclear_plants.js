fec5b3483573e7e9c6aaba7c0046e52fc7d574ab
semtexh <72455541+semtexh@users.noreply.github.com>
2025-06-19 11:02:23 +0300

    Add files via upload

---
+++ embedded_nuclear_plants.js
@@ -0,0 +1,112 @@
+const embeddedPlantData = {
+  "features": [
+    {
+      "properties": {
+        "Name": "Bushehr Nuclear Plant",
+        "Country": "Iran"
+      },
+      "geometry": {
+        "coordinates": [
+          50.88,
+          28.82
+        ]
+      }
+    },
+    {
+      "properties": {
+        "Name": "Arak IR-40",
+        "Country": "Iran"
+      },
+      "geometry": {
+        "coordinates": [
+          49.25,
+          34.53
+        ]
+      }
+    },
+    {
+      "properties": {
+        "Name": "Dimona Nuclear Facility",
+        "Country": "Israel"
+      },
+      "geometry": {
+        "coordinates": [
+          35.03,
+          31.05
+        ]
+      }
+    },
+    {
+      "properties": {
+        "Name": "Leningrad NPP",
+        "Country": "Russia"
+      },
+      "geometry": {
+        "coordinates": [
+          29.03,
+          59.98
+        ]
+      }
+    },
+    {
+      "properties": {
+        "Name": "Kursk NPP",
+        "Country": "Russia"
+      },
+      "geometry": {
+        "coordinates": [
+          35.62,
+          51.67
+        ]
+      }
+    },
+    {
+      "properties": {
+        "Name": "Grafenrheinfeld NPP",
+        "Country": "Germany"
+      },
+      "geometry": {
+        "coordinates": [
+          10.17,
+          50.03
+        ]
+      }
+    },
+    {
+      "properties": {
+        "Name": "Isar Nuclear Power Plant",
+        "Country": "Germany"
+      },
+      "geometry": {
+        "coordinates": [
+          12.29,
+          48.64
+        ]
+      }
+    },
+    {
+      "properties": {
+        "Name": "Hinkley Point B",
+        "Country": "United Kingdom"
+      },
+      "geometry": {
+        "coordinates": [
+          -3.15,
+          51.21
+        ]
+      }
+    },
+    {
+      "properties": {
+        "Name": "Sizewell B",
+        "Country": "United Kingdom"
+      },
+      "geometry": {
+        "coordinates": [
+          1.62,
+          52.21
+        ]
+      }
+    }
+  ]
+};
